175da332c4731b63fab3b9c47f7a67ff1686fa13
Jay Modi <jaymodi@moweb.in>
2026-08-26 00:45:47 +0530

    fix: promotions controller route typo

--- src/promotions/promotions.controller.ts
+++ src/promotions/promotions.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { PromotionsService } from './promotions.service';
 
-@Controller('api/promotions')
+@Controller('promotions')
 export class PromotionsController {
   constructor(private readonly promotionsService: PromotionsService) {}
 
